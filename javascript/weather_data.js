94193a37b1464dccd4a295bda49c701f9f69234c
tsimmi <gilgamex@gmail.com>
2023-10-31 18:16:07 +0000

    Updated weather data;

--- javascript/weather_data.js
+++ javascript/weather_data.js
@@ -1,10 +1,6 @@
 var beach = "Praia Pedras do corgo";
 var weatherData = [
     {
-        "timestamp": 1698771600, 
-        "niceness": 0
-    }, 
-    {
         "timestamp": 1698775200, 
         "niceness": 0
     }, 
@@ -38,43 +34,43 @@ var weatherData = [
     }, 
     {
         "timestamp": 1698804000, 
-        "niceness": 0
+        "niceness": 0.046000000000000006
     }, 
     {
         "timestamp": 1698807600, 
-        "niceness": 0.09900000000000002
+        "niceness": 0.049999999999999975
     }, 
     {
         "timestamp": 1698811200, 
-        "niceness": 0
+        "niceness": 0.0019999999999999463
     }, 
     {
         "timestamp": 1698814800, 
-        "niceness": 0.24
+        "niceness": 0.183
     }, 
     {
         "timestamp": 1698818400, 
-        "niceness": 0.275
+        "niceness": 0.297
     }, 
     {
         "timestamp": 1698822000, 
-        "niceness": 0.409
+        "niceness": 0.285
     }, 
     {
         "timestamp": 1698825600, 
-        "niceness": 0.47200000000000003
+        "niceness": 0.32399999999999995
     }, 
     {
         "timestamp": 1698829200, 
-        "niceness": 0.4119999999999999
+        "niceness": 0.32
     }, 
     {
         "timestamp": 1698832800, 
-        "niceness": 0.24
+        "niceness": 0.207
     }, 
     {
         "timestamp": 1698836400, 
-        "niceness": 0.013
+        "niceness": 0.10599999999999994
     }, 
     {
         "timestamp": 1698840000, 
@@ -122,15 +118,15 @@ var weatherData = [
     }, 
     {
         "timestamp": 1698879600, 
-        "niceness": 0.01799999999999997
+        "niceness": 0.018666666666666623
     }, 
     {
         "timestamp": 1698883200, 
-        "niceness": 0.014000000000000056
+        "niceness": 0.017666666666666705
     }, 
     {
         "timestamp": 1698886800, 
-        "niceness": 0.017666666666666705
+        "niceness": 0.022000000000000002
     }, 
     {
         "timestamp": 1698890400, 
@@ -158,7 +154,7 @@ var weatherData = [
     }, 
     {
         "timestamp": 1698912000, 
-        "niceness": 0.0
+        "niceness": 0
     }, 
     {
         "timestamp": 1698915600, 
@@ -174,22 +170,26 @@ var weatherData = [
     }, 
     {
         "timestamp": 1698926400, 
-        "niceness": 0.0
-    }, 
-    {
-        "timestamp": 1698930000, 
         "niceness": 0
     }, 
     {
-        "timestamp": 1698933600, 
+        "timestamp": 1698930000, 
         "niceness": 0.0
+    }, 
+    {
+        "timestamp": 1698933600, 
+        "niceness": 0
     }, 
     {
         "timestamp": 1698937200, 
-        "niceness": 0.0
+        "niceness": 0
     }, 
     {
         "timestamp": 1698940800, 
-        "niceness": 0.0
+        "niceness": 0
+    }, 
+    {
+        "timestamp": 1698944400, 
+        "niceness": 0
     }
 ];
