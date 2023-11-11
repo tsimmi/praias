eaafef98fabaa842d1608c2a284d6ebc610db55c
tsimmi <gilgamex@gmail.com>
2023-11-11 16:16:07 +0000

    Updated weather data;

--- javascript/weather_data.js
+++ javascript/weather_data.js
@@ -1,10 +1,6 @@
 var beach = "Praia Pedras do corgo";
 var weatherData = [
     {
-        "timestamp": 1699714800, 
-        "niceness": 0
-    }, 
-    {
         "timestamp": 1699718400, 
         "niceness": 0
     }, 
@@ -190,6 +186,10 @@ var weatherData = [
     }, 
     {
         "timestamp": 1699884000, 
+        "niceness": 0
+    }, 
+    {
+        "timestamp": 1699887600, 
         "niceness": 0
     }
 ];
